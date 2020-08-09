7dbe14ef49bfd928883fb0365e22b04d549bfcf6
Emiliano Alvarez <emilianoalvarez91@gmail.com>
2020-08-09 20:49:23 +0200

    Create command line tool project

---
+++ xccovAnalizer/xccovAnalizer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xccovAnalizer
+//
+//  Created by Emiliano Alvarez on 09/08/2020.
+//  Copyright © 2020 Emiliano Alvarez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
